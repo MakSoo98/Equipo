98860dcfc468027f38e76355ca4addc96ba35648
Alex <fernandez21392@iesmarenostrum.com>
2022-03-10 20:45:52 +0100

    .

--- EX220310/TestAlex/Program.cs
+++ EX220310/TestAlex/Program.cs
@@ -8,9 +8,34 @@ namespace TestAlex
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void test1()
         {
             ISpell s = new Spell();
+        }
+
+        public static void test2()
+        {
+            IPokemon p = new WaterPokemon();
+        }
+
+        public static void test3()
+        {
+            ISpell s = new Spell();
+            IPokemon p = new WaterPokemon();
+
+            for(int i = 0; i < 10; i++)
+            {
+                int n = p.AddSpell().Count
+                
+                    if(n =! 0)
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            
         }
 
     }
