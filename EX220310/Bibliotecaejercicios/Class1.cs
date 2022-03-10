28feb75a5aa1c5044c9aa37427ffc3d5d8c050d9
MakSoo98 <marcosferrerhernandez@gmail.com>
2022-03-10 19:40:40 +0100

    .
    .

---
+++ EX220310/Bibliotecaejercicios/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Bibliotecaejercicios
+{
+    public class Spell
+    {
+        private string name;
+        private string description;
+        private int power_consumption;
+        private type stype;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public int Power_consumption
+        {
+            get { return power_consumption; }
+            set { power_consumption = value; }
+        }
+        public type GetType
+        {
+            get { return stype; }
+            set { stype = value; }
+        }
+        public enum type
+        {
+            FIRE,
+            PLANT,
+            WATER
+        }
+        public Spell(string Name, string Description, int Power_consumption, type GetType)
+        {
+            name = Name;
+            description = Description;
+            power_consumption = Power_consumption;
+            stype = GetType;
+        }
+    }
+}
